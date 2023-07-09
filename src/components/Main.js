16593a500dcbc186db3bf900da6b4c6fb8f16a54
Hailex <kshitij.singh.bisht.174sis@gmail.com>
2023-07-09 20:48:01 +0530

    Added the files

---
+++ src/components/Main.js
@@ -0,0 +1,55 @@
+import React from "react"
+
+export default function Main(){
+
+    const [list, setList] = React.useState([])
+    const [input, setInput] = React.useState()
+
+    function handleSubmit(e){
+        e.preventDefault()
+        setList(prev => {
+            return [
+                ...prev,
+                input
+            ]
+        })
+        setInput("")
+    }
+
+    function handleChange(e){
+        setInput(e.target.value)
+    }
+
+    const val6 = list.map(x =>  
+        <li className="main--list">
+            {x}
+            <button onClick={() => toRemove(x)}>X</button>
+        </li>
+    )
+
+    return (
+        <div className="main">
+            <h1 className="main--heading">Items to Buy</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                type= "text"
+                class= "main--input"
+                name= "item"
+                placeholder= "Enter a new item"
+                value= {input}
+                onChange= {handleChange} />
+                <button className="main--btn">Add</button>
+            </form>
+            <div>
+                {val6}
+            </div>
+        </div>
+    )
+
+    function toRemove(item){
+        const val7 = list.filter((x) => 
+        {return x !== item}
+        )
+        setList(val7)
+    }
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Navbar(){
+    return (
+        <div className="nav">
+            <h1 className="nav--heading">SHOPPING LIST</h1>
+            <hr></hr>
+        </div>
+    )
+}
